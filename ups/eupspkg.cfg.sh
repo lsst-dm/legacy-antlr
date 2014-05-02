f92f4d798ef56c45e8fbd4f8991f2cdf5dff4b9e
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-05-02 23:29:33 +0200

    FIX : antlr.jar was deployed to wrong place

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,11 +1,12 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
+PKGDIR=$PWD
 CONFIGURE_OPTIONS="--disable-examples --prefix=$PREFIX"
 
 install()
 {
 	default_install
 
-	# do not erase antlr.jar if it has been successfully built
-	cp -n antlr.jar  $PREFIX/antlr.jar
+	# do not erase lib/antlr.jar if it has been successfully built
+	cp -n ${PKGDIR}/antlr.jar  ${PREFIX}/lib/
 }
