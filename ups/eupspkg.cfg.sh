68f9a71138b228d6ed49c7021502d168656b056f
Mario Juric <mjuric@lsst.org>
2014-12-07 22:07:04 -0800

    ups/eupspkg.cfg.sh: simplify and make OS X compatible

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,12 +1,3 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-PKGDIR=$PWD
 CONFIGURE_OPTIONS="--prefix=$PREFIX --disable-examples --enable-verbose --with-cxxflags=-fPIC"
-
-install()
-{
-	default_install
-
-	# do not erase lib/antlr.jar if it has been successfully built
-	cp -n ${PKGDIR}/antlr.jar  ${PREFIX}/lib/
-}
